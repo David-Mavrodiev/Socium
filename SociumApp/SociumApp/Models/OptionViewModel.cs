c6f3fa131675a2b85341c7c26a3014c349b5732b
David-Mavrodiev <David-Mavrodiev>
2017-03-18 15:38:49 -0700

    Added templates

---
+++ SociumApp/SociumApp/Models/OptionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SociumApp.Models
+{
+    public class OptionViewModel
+    {
+        string Description { get; set; }
+
+        int VotesCount { get; set; }
+    }
+}
---
+++ SociumApp/SociumApp/Models/QuestionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SociumApp.Models
+{
+    public class QuestionViewModel
+    {
+        public string OwnerUsername { get; set; }
+
+        public string Title { get; set; }
+
+        public List<OptionViewModel> Options { get; set; }
+    }
+}
